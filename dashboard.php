851fd1e6e665fc3d341e3eef3463f6be5e4784c7
Adedamola Rioland <riolandadedamola@gmail.com>
2021-09-25 14:42:47 +0100

    dashboard-loop

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Snippet - BBBootstrap</title>
+    <title>Dashboard</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
     <script type='text/javascript' src=''></script>
@@ -51,8 +51,9 @@
         align-items: center;
         justify-content: space-between;
         padding: 0 1rem;
-        background-color: var(--white-color);
-        z-index: var(--z-fixed);
+        background-color: #2F2F2F;
+            /* var(--white-color); */
+            z-index: var(--z-fixed);
         transition: .5s
     }
 
@@ -81,7 +82,7 @@
         left: -30%;
         width: var(--nav-width);
         height: 100vh;
-        background-color: var(--first-color);
+        background-color: #2F2F2F;
         padding: .5rem 1rem 0 0;
         transition: .5s;
         z-index: var(--z-fixed)
@@ -151,7 +152,8 @@
         left: 0;
         width: 2px;
         height: 32px;
-        background-color: var(--white-color)
+        background-color: #2F2F2F;
+        /* var(--white-color) */
     }
 
     .height-100 {
@@ -166,7 +168,9 @@
 
         .header {
             height: calc(var(--header-height) + 1rem);
-            padding: 0 2rem 0 calc(var(--nav-width) + 2rem)
+            padding: 0 2rem 0 calc(var(--nav-width) + 2rem);
+            background:#2F2F2F;
+
         }
 
         .header_img {
@@ -195,6 +199,14 @@
     a {
         text-decoration: none;
     }
+
+    .rcorners1 {
+        border-radius: 25px;
+        background: var(--white-color);
+        padding: 20px;
+        width: 50px;
+        height: 40px;
+    }
     </style>
 </head>
 
@@ -203,41 +215,84 @@
     <body id="body-pd">
         <header class="header" id="header">
             <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i>
-
-                noavibnbxbnxbxnx
             </div>
             <!-- profile image -->
-            <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt="">
-
+            <div class="rcorners1">
+                <label>A</label>
             </div>
+            <!-- or this -->
+            <!-- <div class="header_img">
+
+                <img src="https://i.imgur.com/hczKIze.jpg" alt="">
+            </div> -->
+
         </header>
         <div class="l-navbar" id="nav-bar">
             <nav class="nav">
                 <div>
+                    <!-- sitename -->
                     <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
                             class="nav_logo-name">Riotech UI</span>
                     </a>
+
                     <div class="nav_list">
+                        <!-- dashboard -->
                         <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i>
-                            <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i
-                                class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#"
-                            class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span
-                                class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i
-                                class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a
-                            href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span
-                                class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i
-                                class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
+                            <span class="nav_name">Dashboard</span> </a>
+                        <!-- deposite -->
+                        <a href="#" class="nav_link">
+                            <!-- <i class='bx bx-user nav_icon'> -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-wallet" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+                            </svg>
+                            </i> <span class="nav_name">Deposit</span>
+                        </a>
+                        <!-- withdraw -->
+                        <a href="#" class="nav_link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-folder2" viewBox="0 0 16 16">
+                                <path
+                                    d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z" />
+                            </svg>
+                            </i> <span class="nav_name">Withdraw</span> </a>
+                        <!-- Invest -->
+                        <a href="#" class="nav_link"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+                            </svg> <span class="nav_name">Invest</span> </a>
+                        <!-- transaction -->
+                        <a href="#" class="nav_link"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-list-check" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                            </svg></i> <span class="nav_name">Transaction</span>
+                        </a>
+                        <!-- profile -->
+                        <a href="#" class="nav_link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg> <span class="nav_name">Profile</span> </a>
+
                     </div>
-                </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span
+                    <!-- end of side bar -->
+                    <!-- logout div -->
+                </div>
+                <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span
                         class="nav_name">SignOut</span> </a>
             </nav>
         </div>
         <!--Container Main start-->
         <div class="height-100 bg-light">
             <?php
-          require("./pages/user.php")
+         require "./pages/home.php";
+         require "./coinshow.php";
 
-            ?>
+             ?>
         </div>
         <!--Container Main end-->
         <script type='text/javascript'
--- pages/home.php
+++ pages/home.php
@@ -1,305 +1,154 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Snippet - BBBootstrap</title>
-    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
-    <link href='' rel='stylesheet'>
-    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-    body {
-        background-color: #f9f9fa
+    .act {
+        height: 400px;
+        background: #2F2F2F;
+        padding-top: 150px;
+        padding-left: 78px;
+        padding-right: 78px;
     }
 
-    .flex {
-        -webkit-box-flex: 1;
-        -ms-flex: 1 1 auto;
-        flex: 1 1 auto
+    .rcorners2 {
+        border-radius: 15px 50px 30px 5px;
+        background: white;
+        padding: 20px;
+        width: 200px;
+        height: 150px;
     }
 
-    @media (max-width:991.98px) {
-        .padding {
-            padding: 1.5rem
-        }
+    .sp1 {
+        /* margin-right: 34px; */
+
     }
 
-    @media (max-width:767.98px) {
-        .padding {
-            padding: 1rem
-        }
-    }
-
-    .padding {
-        padding: 5rem
-    }
-
-    .card {
-        background: #fff;
-        border-width: 0;
-        border-radius: .25rem;
-        box-shadow: 0 1px 3px rgba(0, 0, 0, .05);
-        margin-bottom: 1.5rem
-    }
-
-    .card {
-        position: relative;
+    .camp1 {
         display: flex;
-        flex-direction: column;
-        min-width: 0;
-        word-wrap: break-word;
-        background-color: #fff;
-        background-clip: border-box;
-        border: 1px solid rgba(19, 24, 44, .125);
-        border-radius: .25rem
+        justify-content: space-around;
     }
-
-    .card-header {
-        padding: .75rem 1.25rem;
-        margin-bottom: 0;
-        background-color: rgba(19, 24, 44, .03);
-        border-bottom: 1px solid rgba(19, 24, 44, .125)
-    }
-
-    .card-header:first-child {
-        border-radius: calc(.25rem - 1px) calc(.25rem - 1px) 0 0
-    }
-
-    card-footer,
-    .card-header {
-        background-color: transparent;
-        border-color: rgba(160, 175, 185, .15);
-        background-clip: padding-box
-    }
+    .rcorners3 {
+  border-radius: 25px;
+  background: #73AD21;
+  padding-top:4px ;
+  /* padding: 20px; */
+  width: 40px;
+  height:40px;
+  /* align-items: center; */
+  text-align: center;
+  /* display: flex;
+  justify-content: center; */
+}
     </style>
+
 </head>
 
-<body oncontextmenu='return false' class='snippet-body'>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
-    <script>
-    $(document).ready(function() {
-        var ctx = $("#chart-line");
-        var myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
-                datasets: [{
-                    data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-                    label: "Africa",
-                    borderColor: "#458af7",
-                    fill: false
-                }, {
-                    data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-                    label: "Asia",
-                    borderColor: "#8e5ea2",
-                    fill: false
-                }, {
-                    data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-                    label: "Europe",
-                    borderColor: "#3cba9f",
-                    fill: false
-                }]
-            },
-            options: {
-                title: {
-                    display: true,
-                    text: 'World population per region (in millions)'
-                }
-            }
-        });
-    });
-    </script>
-    <div class="page-content page-container" id="page-content">
-        <div class="padding">
-            <div class="row">
-                <div class="container-fluid d-flex justify-content-center">
-                    <div class="col-sm-8 col-md-6">
-                        <div class="card">
-                            <div class="card-header">Line chart</div>
-                            <div class="card-body" style="height: 420px">
-                                <div class="chartjs-size-monitor"
-                                    style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
-                                    <div class="chartjs-size-monitor-expand"
-                                        style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
-                                        <div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0">
-                                        </div>
-                                    </div>
-                                    <div class="chartjs-size-monitor-shrink"
-                                        style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
-                                        <div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
-                                    </div>
-                                </div> <canvas id="chart-line" width="299" height="200" class="chartjs-render-monitor"
-                                    style="display: block; width: 299px; height: 200px;"></canvas>
-                            </div>
-                        </div>
+<body>
+
+    <div class="act">
+
+        <div style="display: flex; justify-content:space-between; ">
+        <!-- 1 -->
+            <div class="rcorners2 sp1">
+                <div class="camp1">
+                    <div>
+                        <div style="font-size:25px; font-weight:bold;" >Balance</div>
+                        <div style="font-weight:bold;" >USD </div>
                     </div>
-                    <!doctype html>
-                    <html>
+                    <div class="rcorners3">
+                        
 
-                    <head>
-                        <meta charset='utf-8'>
-                        <meta name='viewport' content='width=device-width, initial-scale=1'>
-                        <title>Snippet - BBBootstrap</title>
-                        <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
-                            rel='stylesheet'>
-                        <link href='' rel='stylesheet'>
-                        <script type='text/javascript'
-                            src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-                        <style>
-                        body {
-                            background-color: #f9f9fa
-                        }
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"
+                            class="bi bi-wallet" viewBox="0 0 16 16">
+                            <path
+                                d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+                        </svg>
+                        
+                    </div>
+                </div>
+                <!-- pin down -->
+                <div class="camp2">
+                    eeee
+                </div>
+            </div>
+            <!-- 2 -->
+            <div class="rcorners2 sp1">
+                <div class="camp1">
+                    <div>
+                        <div style="font-size:25px; font-weight:bold;" >Balance</div>
+                        <div style="font-weight:bold;" >USD </div>
+                    </div>
+                    <div class="rcorners3" style="background: #FFC300;">
+                        
 
-                        .flex {
-                            -webkit-box-flex: 1;
-                            -ms-flex: 1 1 auto;
-                            flex: 1 1 auto
-                        }
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"
+                            class="bi bi-wallet" viewBox="0 0 16 16">
+                            <path
+                                d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+                        </svg>
+                        
+                    </div>
+                </div>
+                <!-- pin down -->
+                <div class="camp2">
+                    eeee
+                </div>
+            </div>
+            <!-- 3 -->
+            <div class="rcorners2 sp1">
+                  <div class="camp1">
+                    <div>
+                        <div style="font-size:25px; font-weight:bold;" >Balance</div>
+                        <div style="font-weight:bold;" >USD </div>
+                    </div>
+                    <div class="rcorners3" style="background: #FF5733;">
+                        
 
-                        @media (max-width:991.98px) {
-                            .padding {
-                                padding: 1.5rem
-                            }
-                        }
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"
+                            class="bi bi-wallet" viewBox="0 0 16 16">
+                            <path
+                                d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+                        </svg>
+                        
+                    </div>
+                </div>
+                <!-- pin down -->
+                <div class="camp2">
+                    eeee
+                </div>
+            </div>
+            <!-- 4 -->
+            <div class="rcorners2 sp1">
+              <div class="camp1">
+                    <div>
+                        <div style="font-size:25px; font-weight:bold;" >Balance</div>
+                        <div style="font-weight:bold;" >USD </div>
+                    </div>
+                    <div class="rcorners3" style="background: #900C3F;">
+                        
 
-                        @media (max-width:767.98px) {
-                            .padding {
-                                padding: 1rem
-                            }
-                        }
-
-                        .padding {
-                            padding: 5rem
-                        }
-
-                        .card {
-                            background: #fff;
-                            border-width: 0;
-                            border-radius: .25rem;
-                            box-shadow: 0 1px 3px rgba(0, 0, 0, .05);
-                            margin-bottom: 1.5rem
-                        }
-
-                        .card {
-                            position: relative;
-                            display: flex;
-                            flex-direction: column;
-                            min-width: 0;
-                            word-wrap: break-word;
-                            background-color: #fff;
-                            background-clip: border-box;
-                            border: 1px solid rgba(19, 24, 44, .125);
-                            border-radius: .25rem
-                        }
-
-                        .card-header {
-                            padding: .75rem 1.25rem;
-                            margin-bottom: 0;
-                            background-color: rgba(19, 24, 44, .03);
-                            border-bottom: 1px solid rgba(19, 24, 44, .125)
-                        }
-
-                        .card-header:first-child {
-                            border-radius: calc(.25rem - 1px) calc(.25rem - 1px) 0 0
-                        }
-
-                        card-footer,
-                        .card-header {
-                            background-color: transparent;
-                            border-color: rgba(160, 175, 185, .15);
-                            background-clip: padding-box
-                        }
-                        </style>
-                    </head>
-
-                    <body oncontextmenu='return false' class='snippet-body'>
-                        <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'>
-                        </script>
-                        <script>
-                        $(document).ready(function() {
-                            var ctx = $("#chart-line");
-                            var myLineChart = new Chart(ctx, {
-                                type: 'line',
-                                data: {
-                                    labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999,
-                                        2050],
-                                    datasets: [{
-                                        data: [86, 114, 106, 106, 107, 111, 133, 221, 783,
-                                            2478],
-                                        label: "Africa",
-                                        borderColor: "#458af7",
-                                        fill: false
-                                    }, {
-                                        data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700,
-                                            5267
-                                        ],
-                                        label: "Asia",
-                                        borderColor: "#8e5ea2",
-                                        fill: false
-                                    }, {
-                                        data: [168, 170, 178, 190, 203, 276, 408, 547, 675,
-                                            734],
-                                        label: "Europe",
-                                        borderColor: "#3cba9f",
-                                        fill: false
-                                    }]
-                                },
-                                options: {
-                                    title: {
-                                        display: true,
-                                        text: 'World population per region (in millions)'
-                                    }
-                                }
-                            });
-                        });
-                        </script>
-                        <div class="page-content page-container" id="page-content">
-                            <div class="padding">
-                                <div class="row">
-                                    <div class="container-fluid d-flex justify-content-center">
-                                        <div class="col-sm-8 col-md-6">
-                                            <div class="card">
-                                                <div class="card-header">Line chart</div>
-                                                <div class="card-body" style="height: 420px">
-                                                    <div class="chartjs-size-monitor"
-                                                        style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
-                                                        <div class="chartjs-size-monitor-expand"
-                                                            style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
-                                                            <div
-                                                                style="position:absolute;width:1000000px;height:1000000px;left:0;top:0">
-                                                            </div>
-                                                        </div>
-                                                        <div class="chartjs-size-monitor-shrink"
-                                                            style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
-                                                            <div
-                                                                style="position:absolute;width:200%;height:200%;left:0; top:0">
-                                                            </div>
-                                                        </div>
-                                                    </div> <canvas id="chart-line" width="299" height="200"
-                                                        class="chartjs-render-monitor"
-                                                        style="display: block; width: 299px; height: 200px;"></canvas>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <script type='text/javascript'
-                            src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'>
-                        </script>
-                        <script type='text/javascript'></script>
-                    </body>
-
-                    </html>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"
+                            class="bi bi-wallet" viewBox="0 0 16 16">
+                            <path
+                                d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+                        </svg>
+                        
+                    </div>
+                </div>
+                <!-- pin down -->
+                <div class="camp2">
+                    eeee
                 </div>
             </div>
         </div>
+
     </div>
-    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'>
-    </script>
-    <script type='text/javascript'></script>
+
+
+
 </body>
 
 </html>
